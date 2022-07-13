ef5f8902f60f6d278e391c2ffde16dd812d56bd9
emmetore <emmetore@gmail.com>
2022-07-13 10:38:53 -0600

    v2 - fixed error. now fully functional.

--- src/rest/HousesApi.js
+++ src/rest/HousesApi.js
@@ -1,20 +1,24 @@
-const HOUSES_ENDPOINT = 'https://ancient-taiga-31359.herokuapp.com/api/houses';
+// const HOUSES_ENDPOINT = 'https://ancient-taiga-31359.herokuapp.com/api/houses';
+const HOUSES_ENDPOINT = 'https://62b51cffda3017eabb14e834.mockapi.io/houseProject';
 
 class HousesApi {
     get = async () => {
         try {
             const resp = await fetch(HOUSES_ENDPOINT);
+            console.log(resp);
             const data = await resp.json();
+            console.log(data);
             return data;
+
         } catch (e) {
             console.log('Looks like fetchHouses had an issue', e);
         }
     }
 
-    pub = async (house) => {
+    put = async (house) => {
         try {
             const resp = await fetch(`${HOUSES_ENDPOINT}/${house._id}`, {
-                method: 'PUT',
+                method: "PUT",
                 headers: {
                     'Content-Type': 'application/json'
                 },
